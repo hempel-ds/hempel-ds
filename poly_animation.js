b5a4709eb7eb3cc83ed464966063887e64b40536
hempel-ds <124683497+hempel-ds@users.noreply.github.com>
2024-06-13 14:37:18 +0200

    Use animator

--- poly_animation.js
+++ poly_animation.js
@@ -33,40 +33,87 @@ function getPresentationMode() {
 
 
 /**
- * Animate the poly viewer.
+ * Mesh animation.
+ *
+ * Cycles a sequence of meshes and renders them.
+ *
+ * To mitigate loading times of mesh files, which might be larger than you
+ * expect, the animation waits until a frame is loaded and then triggers the
+ * next frame. This is not correct from an animation point of view, but the
+ * results a visible at the first cycle. Otherwise there are some ugly gaps
+ * in the sequence.
  */
-function animate(viewer, delay, type, meshes, start, end, step=0) {
-    if(-1 == end) {
-        // play all the meshes
-        end = meshes.length - 1;
+class Animation {
+    /**
+     * Parameters:
+     *    viewer: viewer the mesh is rendered with (Google Mesh Viewer)
+     *    delay: delay between two frames (in seconds)
+     *    type: type of the animation; use "mesh" for a single frame.
+     *    meshes: all the meshes which could be rendered
+     *    start: first mesh to render (index)
+     *    end: last mesh to render (index)
+     */
+    constructor(viewer, delay, type, meshes, start, end) {
+        this.viewer = viewer;
+        this.delay = delay;
+        this.type = type;
+        this.meshes = meshes;
+        this.start = start;
+        this.end = end;
+        this.step = start;
+        
+        if(-1 == end) {
+	    // play all the meshes
+            this.end = meshes.length - 1;
+        }
+        
+        this.num_meshes = end - start + 1;
     }
     
-    const num_meshes = end - start + 1;
+    /**
+     * Run the animation.
+     *
+     * This method should only be called once per viewer. A callback is
+     * registered for the viewer (on load) to get a nice and smooth animation.
+     */
+    run() {
+        if(this.num_meshes <= 0) {
+	    // nothing to show at all
+    	    return;
+        }
     
-    if(num_meshes <= 0) {
-        // nothing to show at all
-        return;
+        // just change the source of the viewer to display a new mesh
+        const mesh = this.meshes[this.step];
+    
+        if(this.meshes.length == 1) {
+	    // there is only a single element to show, no need to animate
+            this.viewer.src = mesh;
+	    return;
+        }
+        
+        // set the new mesh
+        let that = this;
+        this.viewer.addEventListener(
+            "load",
+            function() {
+                setTimeout(that.nextImage, 1000 * that.delay, that);
+            });
+        
+        this.viewer.src = mesh;
     }
     
-    // just change the source of the viewer to display a new mesh
-    const mesh = meshes[step % meshes.length];
-    viewer.setAttribute("src", mesh);
-    // viewer.src = mesh;
-
-    if(meshes.length == 1) {
-        // there is only a single element to show, no need to animate
-        return;
+    /**
+     * Called when the next frame is enabled for rendering.
+     */
+    nextImage(that) {
+        that.step = that.step + 1;
+        if(that.step > that.end) {
+            that.step = that.start;
+        }
+        
+        that.viewer.src = that.meshes[that.step];
     }
-    
-    
-    // loop
-    step = step + 1;
-    if(step > end) {
-        step = start;
-    }
-    
-    setTimeout(animate, 1000 * delay, viewer, delay, type, meshes, start, end, step);
-}
+};
 
 
 $(document).ready(function() {
@@ -87,14 +134,16 @@ $(document).ready(function() {
             const meshes = configuration.meshes;
             const start = configuration.start;
             const end = configuration.end;
-        
-            animate(viewer, delay, type, meshes, start, end, start);
+    
+            var animation = new Animation(viewer, delay, type, meshes, start, end, start);
+            animation.run();
         }).fail(function() {
             console.log("Failed loading configuration: " + configuration);
         });
     } else if(mode == MODE_MESH) {
         // Mesh is loaded from a mesh file directly.
-        animate(viewer, 0.0, "mesh", [name + ".glb"], 0, 0, 0);
+        var animation = new Animation(viewer, 0, "mesh", [name + ".glb"], 0, 0, 0);
+        animation.run();
     } else {
         console.log("Unknown mode: " + mode);
     }
